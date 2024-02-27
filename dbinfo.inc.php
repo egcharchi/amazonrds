322e19b58c05426335edabf82dbfb81f96ee9fc4
egcharchi <eric.charchi@student.moringaschool.com>
2024-02-27 18:51:03 +0300

    Added sample php files

---
+++ dbinfo.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_SERVER', 'db_instance_endpoint'); //insert endpoint here
+define('DB_USERNAME', 'tutorial_user'); //dbuser here
+define('DB_PASSWORD', 'master password'); //master pw here
+define('DB_DATABASE', 'sample');
+?>
